d5d4c5f2eb4d35169351c64944022ed3bf65b8e0
benttoenail <maxrose8841@gmail.com>
2016-10-20 20:14:59 -0400

    instatiate with params:progress

--- Assets/Scripts_custom/CloudButtonList/CloudBlock.cs
+++ Assets/Scripts_custom/CloudButtonList/CloudBlock.cs
@@ -1,12 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CloudBlock : MonoBehaviour {
 
+    TextMesh text;
+    //List<string> cloudName = new List<string>();
+    string cloudName;
+
+    void OnEnable()
+    {
+        CloudButtonHandler.BlockCreated += InitBlock;
+    }
+
+
+    void InitBlock(string s)
+    {
+        cloudName = s;
+        Debug.Log("Object created");
+    }
+
+    void Awake()
+    {
+        
+    }
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        text = GetComponent<TextMesh>();
+        text.text = cloudName;
+    }
 	
 	// Update is called once per frame
 	void Update () {
--- Assets/Scripts_custom/CloudButtonList/CloudButtonHandler.cs
+++ Assets/Scripts_custom/CloudButtonList/CloudButtonHandler.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class CloudButtonHandler : MonoBehaviour {
 
@@ -7,18 +9,36 @@ public class CloudButtonHandler : MonoBehaviour {
     [SerializeField]
     GameObject blockPrefab;
 
+    //string[] names;
+    List<string> names = new List<string>();
+    GameObject[] objects;
+
+    public delegate void CloudListEventHandler(string s);
+    public static event CloudListEventHandler BlockCreated;
+
 	// Use this for initialization
 	void Start () {
 
         float y = 0;
         Vector3 handlerPos = transform.position;
+
         foreach(Transform c in DataSet.transform)
         {
-            Debug.Log("here you go..." + c.transform.gameObject.name);
             GameObject block = Instantiate(blockPrefab, new Vector3(handlerPos.x, handlerPos.y + y, handlerPos.z), Quaternion.identity) as GameObject;
+            BlockCreated(c.gameObject.name);
             block.transform.SetParent(gameObject.transform);
+            Debug.Log(c.gameObject.name); // This is Correct!
+
+            names.Add(c.gameObject.name);
             y = y - 50;
         }
+
+        /*
+        for(int i = 0; i < names.Count; i++)
+        {
+            BlockCreated(names[i]);
+        }
+        */
 
 	}
 	
